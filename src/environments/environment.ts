aaa2a90c75c6724be4bcd64bb86a06054f670407
Shashikumar Selvaraj <shashikr88.sap@gmail.com>
2024-07-08 20:43:39 +0200

    added environment entries

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000', // Adjust the URL to your backend
+  production: true,
+  apiUrl: 'https://node-book-api.vercel.app/api/v1',
 };
